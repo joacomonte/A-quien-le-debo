5494d6a2420c36c121e6e081e9eb7c4929e3b91c
Joaquin Montesano <joaquin@moabits.com>
2024-11-23 13:16:21 -0300

    ahora?

--- src/app/api/event/[eventId]/members/[memberId]/balance/route.ts
+++ src/app/api/event/[eventId]/members/[memberId]/balance/route.ts
@@ -2,10 +2,19 @@ import { createSuccessResponse } from "@/app/api/_utils/functions";
 import { getMemberBalance } from "./functions";
 
 
-export async function POST(request: Request, { params }: { params: { eventId: string; memberId: string } }) {
-  const { eventId, memberId } = params;
+type Params = {
+  params: {
+    eventId: string;
+    memberId: string;
+  };
+};
 
-  const getMemberBalanceResponse = await getMemberBalance(eventId, memberId);
+export async function POST(req: Request, params: Params) {
+  const { eventId } = params.params;
+  const { memberId } = params.params;
+
+  const getMemberBalanceResponse = await getMemberBalance(eventId, memberId) ;
 
   return createSuccessResponse(getMemberBalanceResponse, "OK");
+  
 }
